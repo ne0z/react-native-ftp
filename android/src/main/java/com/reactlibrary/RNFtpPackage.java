fefa44a9564418138eb62baa58190e5b27e3f85a
Danang Heriyadi <danang@crrsecurity.com>
2018-09-08 23:37:51 +0700

    Fix error caused by Override on line 19

--- android/src/main/java/com/reactlibrary/RNFtpPackage.java
+++ android/src/main/java/com/reactlibrary/RNFtpPackage.java
@@ -15,8 +15,7 @@ public class RNFtpPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNFtpModule(reactContext));
     }
-
-    @Override
+    
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
